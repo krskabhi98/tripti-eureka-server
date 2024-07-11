9f6035ab86f7fea4c0c275b169f5a3e0b71fd023
akuma518 <kr.skabhi@gmail.com>
2024-07-12 02:13:49 +0530

    first commit

---
+++ src/main/java/com/tripti/ecommerce/eureka_server/tripti/eureka/server/TriptiEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.tripti.ecommerce.eureka_server.tripti.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriptiEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriptiEurekaServerApplication.class, args);
+	}
+
+}
